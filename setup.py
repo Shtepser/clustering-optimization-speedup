1728b7368bc63b4f839e77c7dd2fbe65101350d0
Anatoliy Poletaev <shtepser@yandex.ru>
2020-06-18 13:57:17 +0300

    Fixed project setup.py

--- setup.py
+++ setup.py
@@ -12,12 +12,16 @@ ext_modules = [
 
 if __name__ == "__main__":
     setup(name='clustering-optimization-speedup',
+          author='Anatoliy Poletaev',
+          author_email='Shtepser@yandex.ru',
           version='0.9',
           packages=find_packages(exclude=['*.pyx', '*.c']),
           ext_modules=ext_modules,
           extras_require={
               "Bundled_optimizer": ['cvxpy==1.0.31']
           },
+          url='https://github.com/Shtepser/clustering-optimization-speedup',
+          long_description='Package for speeding up portfolio optimization using hierarchical clustering',
           python_requires='>=3.8',
           install_requires=['scipy>=1.4.1', 'numpy>=1.18.4']
           )
